535222e11743131e52c125be25d44cd4c83de979
alexerx <1695927232@qq.com>
2022-07-15 17:19:40 +0800

    feat: new nav

---
+++ modules/app/Nav.tsx
@@ -0,0 +1,28 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Box, Button, Flex, Stack, useColorMode } from '@chakra-ui/react';
+import Link from 'next/link';
+
+export const Nav: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Box px={4} zIndex={9999}>
+      <Flex h={20} alignItems={'center'} justifyContent={'space-between'}>
+        <Box>MintVip</Box>
+
+        <Flex alignItems={'center'}>
+          <Stack direction={'row'} spacing={2}>
+            <Button bg="transparent" onClick={toggleColorMode}>
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
+
+            <Link href="/app">
+              <Button size={'lg'} colorScheme={'main'}>
+                Launch App
+              </Button>
+            </Link>
+          </Stack>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
