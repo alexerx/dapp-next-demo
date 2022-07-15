ba44bba07af61cdbd1d69b52d03a7d884f123653
qiangjian <qiangjian@bytedance.com>
2022-07-15 13:24:15 +0800

    feat: home page

---
+++ modules/navs/HomePageNav.tsx
@@ -0,0 +1,33 @@
+import {
+  Box,
+  Flex,
+  Button,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import Link from 'next/link';
+
+export const HomePageNav: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <Box>MintVip</Box>
+
+        <Flex alignItems={'center'}>
+          <Stack direction={'row'} spacing={7}>
+            <Button onClick={toggleColorMode}>
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
+
+            <Link href="/app">
+              <Button colorScheme="purple">Launch App</Button>
+            </Link>
+          </Stack>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
