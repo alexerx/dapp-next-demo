671f50f0e34da1a9ebcc453f291616d5a835bc36
alexerx <1695927232@qq.com>
2022-07-16 18:18:52 +0800

    feat: optimise navbar

--- modules/common/layouts/Navbar/index.tsx
+++ modules/common/layouts/Navbar/index.tsx
@@ -1,12 +1,49 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
+import { throttle } from 'lodash';
+import { useRouter } from 'next/router';
+import { useEffect, useMemo, useState } from 'react';
 import { AppNavbar } from './AppNavbar';
 import { HomeNavBar } from './HomeNavbar';
 
 export const Navbar: React.FC = () => {
+  const bgColor = useColorModeValue('white', 'gray.800');
+
+  const [scrollTop, setScrollTop] = useState(0);
+  const boxShadow = useMemo(() => {
+    if (scrollTop > 10) {
+      return 'md';
+    } else {
+      return undefined;
+    }
+  }, [scrollTop]);
+
+  const { pathname } = useRouter();
+  const navbar = useMemo(() => {
+    if (pathname === '/') {
+      return <HomeNavBar />;
+    } else {
+      return <AppNavbar />;
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    window.addEventListener(
+      'scroll',
+      throttle(() => {
+        setScrollTop(document.documentElement.scrollTop ?? 0);
+      }, 25)
+    );
+  }, []);
+
   return (
-    <Box position={'sticky'} top={0}>
-      <HomeNavBar />
-      <AppNavbar />
+    <Box
+      position={'sticky'}
+      top={0}
+      zIndex={1}
+      bg={bgColor}
+      boxShadow={boxShadow}
+    >
+      {navbar}
     </Box>
   );
 };
